c875ce0413e8a106dc9ba8cfd817601c274f9858
MD ABDUR RAHMAN <mmm.rahman77@gmail.com>
2021-11-11 22:06:36 +0600

    update

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express()
 const port = process.env.PORT || 5000;
 
-/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnelf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`; */
-
-const uri = `mongodb+srv://watchshop:hbOVLjusrAgEtZ62@cluster0.cnelf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cnelf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 app.use(bodyParser.json());
 app.use(cors());
